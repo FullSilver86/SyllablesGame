1937006a0ab5d857867a1a1f51625d021adc9dfb
Przemek <przemyslaw.nowak86@gmail.com>
2023-04-14 13:25:47 +0200

    Testing tts

---
+++ lib/views/home.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          // Background image
+          Image.asset(
+            'assets/images/background.png',
+            fit: BoxFit.cover,
+            height: double.infinity,
+            width: double.infinity,
+          ),
+
+          // Column with buttons in the center
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Image.asset(
+                  'assets/images/home_screen_button1.png',
+                  height: 300,
+                  width: 600,
+                ),
+                // SizedBox(height: 20),
+                // TextButton(
+                //   onPressed: () {},
+                //   child: Text('Button 1'),
+                // ),
+                // SizedBox(height: 10),
+                // TextButton(
+                //   onPressed: () {},
+                //   child: Text('Button 2'),
+                // ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
